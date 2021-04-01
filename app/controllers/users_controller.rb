be3d7e2f7e9f4e677ce7aae1a4485801f70464d6
maeda-seina <ayrton19601027@gmail.com>
2021-04-01 17:44:08 +0900

    devise提出

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def index
+    @users = User.order(:id).page(params[:page]).per(3)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
+  def update; end
+
   def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :postal_code, :address, :introduction)
   end
 end
