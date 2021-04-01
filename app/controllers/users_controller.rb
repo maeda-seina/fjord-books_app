b1be6c2f5e1d30ec74649292bd7b17d489cbd205
maeda-seina <ayrton19601027@gmail.com>
2021-04-01 17:49:07 +0900

    使わないコントローラーアクションを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def update; end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :postal_code, :address, :introduction)
-  end
 end
