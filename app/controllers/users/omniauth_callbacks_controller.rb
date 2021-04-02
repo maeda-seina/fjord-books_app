8d3f22539a4195add129464daf429177b0056c84
maeda-seina <ayrton19601027@gmail.com>
2021-04-02 10:33:25 +0900

    omniauth提出

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Github')
+    else
+      redirect_to new_user_registration_path
+    end
+  end
+end
