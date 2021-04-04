4b11b3a1428f3f3f762c97666030cc3d2bb3eafb
maeda-seina <ayrton19601027@gmail.com>
2021-04-05 08:42:55 +0900

    日報のCRUD機能の大枠作成

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class ReportsController < ApplicationController
+  def index
+    @reports = Report.all
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def edit
+    @report = Report.find(params[:id])
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to @report
+    else
+      render new
+    end
+  end
+
+  def update
+    @report = Report.find(params[:id])
+    if @report.update(report_params)
+      redirect_to @report
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @report = Report.find(params[:id])
+    @report.destroy
+    redirect_to reports_path
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :body)
+  end
+end
