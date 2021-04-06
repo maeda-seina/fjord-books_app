a8dd72588b5e364f0cb9a6cf2ffa039faf8c54e3
maeda-seina <ayrton19601027@gmail.com>
2021-04-06 20:56:49 +0900

    i18nで日本語化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+
   def create
     resource, id = request.path.split('/')[1, 2]
     @commentable = resource.singularize.classify.constantize.find(id)
     @comment = @commentable.comments.build(comment_params)
     @comment.save
-    redirect_to @commentable
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
   end
-
-  def ensure_correct_user; end
 
   private
 
   def comment_params
     params.require(:comment).permit(:body).merge(user_id: current_user.id)
   end
+  
 end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,7 +22,7 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.new(report_params)
     if @report.save
-      redirect_to @report
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render new
     end
@@ -31,7 +31,7 @@ class ReportsController < ApplicationController
   def update
     @report = Report.find(params[:id])
     if @report.update(report_params)
-      redirect_to @report
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render edit
     end
@@ -40,7 +40,7 @@ class ReportsController < ApplicationController
   def destroy
     @report = Report.find(params[:id])
     @report.destroy
-    redirect_to reports_path
+    redirect_to reports_path, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   def ensure_correct_user
