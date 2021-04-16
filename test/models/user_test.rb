2c88b98609a47aebc89c44c92c7821281e179282
maeda-seina <ayrton19601027@gmail.com>
2021-04-16 15:12:41 +0900

    userクラスのテスト作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "#name_or_email" do
+  test '#name_or_email' do
     user = User.new(email: 'foo@example.com', name: '')
-
     assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'FooBar'
+    assert_equal 'FooBar', user.name_or_email
+  end
+
+  test '#follow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+    assert she.followed_by?(me)
+  end
+
+  test '#unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.following?(she)
+    me.follow(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
   end
 end
