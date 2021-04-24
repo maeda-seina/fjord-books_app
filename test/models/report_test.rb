4c5ef0aa7e3a616681a3da64e5c881d0f66482e1
maeda-seina <ayrton19601027@gmail.com>
2021-04-24 22:34:40 +0900

    fixturesのデータをmodelsのreport_test.rbで使用

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,16 +4,16 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable' do
-    alice = User.new(email: 'alice@example.com', name: '')
+    alice = users(:alice)
     bob = User.new(email: 'bob@example.com', name: '')
-    alice_report = alice.reports.new(title: 'alice_report', content: 'alice_report_content')
+    alice_report = reports(:alice_report)
 
     assert alice_report.editable?(alice)
     assert_not alice_report.editable?(bob)
   end
 
   test '#created_on' do
-    alice = User.new(email: 'alice@example.com', name: '')
+    alice = users(:alice)
     alice_report = alice.reports.new(title: 'alice_report', content: 'alice_report_content', created_at: '2020-01-01 11:42:53.052122')
 
     assert_equal '2020/01/01', I18n.l(alice_report.created_on)
